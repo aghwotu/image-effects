7f3a21193b1236c9d69016efb7a7c447ca9d892c
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-12 21:24:25 +0100

    feat: add image crate to load the uploaded image

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use base64::decode;
+use image::load_from_memory;
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 
@@ -9,4 +10,7 @@ pub fn grayscale(encoded_file: &str) {
 
     let base64_to_vector = decode(&encoded_file).unwrap();
     log(&"Image decoded".into());
+
+    let _image = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded".into())
 }
