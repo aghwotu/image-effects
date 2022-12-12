72de5ea631c0584044137ed88030ca3353205513
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-12 18:36:40 +0100

    feat: remove unwanted text from base64 image string

--- public/main.js
+++ public/main.js
@@ -1,1 +1,15 @@
-console.log('test');
+function init() {
+  const input = document.getElementById('upload');
+  const fileReader = new FileReader();
+
+  fileReader.onloadend = () => {
+    let base64 = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
+    console.log(base64);
+  };
+
+  input.addEventListener('change', () => {
+    fileReader.readAsDataURL(input.files[0]);
+  });
+}
+
+init();
