b839ecea699f61299a6174d567da8945669c7dae
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-12 20:39:51 +0100

    feat: decoding the base64 image

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+use base64::decode;
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
-    log(&encoded_file.into());
+    // log(&encoded_file.into());
+    log(&"Greyscale called:".into());
+
+    let base64_to_vector = decode(&encoded_file).unwrap();
+    log(&"Image decoded".into());
 }
