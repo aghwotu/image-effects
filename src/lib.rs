2f4141566f1a322ac7eb350d6ed044eaaadd9015
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-12 21:39:14 +0100

    feat: create buffer for image

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use base64::decode;
 use image::load_from_memory;
+use image::ImageOutputFormat::Png;
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 
@@ -16,4 +17,8 @@ pub fn grayscale(encoded_file: &str) {
 
     image = image.grayscale();
     log(&"Grayscale effect applied".into());
+
+    let mut buffer = vec![];
+    image.write_to(&mut buffer, Png).unwrap();
+    log(&"New image written".into());
 }
